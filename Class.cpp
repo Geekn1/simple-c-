c8826cbb0d9ce95fe088be490dd2dfafc8b3b337
Geekn1 <146921305+Geekn1@users.noreply.github.com>
2023-10-04 12:06:22 +0500

    Add files via upload

---
+++ Class.cpp
@@ -0,0 +1,69 @@
+/*#include<iostream>
+using namespace std;
+class Room {
+    public:
+        double length;
+        double breadth;
+        double height;   
+
+        double calculateArea(){   
+            return length * breadth;
+        }
+
+        double calculateVolume(){   
+            return length * breadth * height;
+        }
+
+};
+int main()
+{
+	Room p;
+	p.breadth=7.9;
+	p.height=8.9;
+	p.length=10.9;
+	cout<<p.calculateArea();
+}*/
+// Program to illustrate the working of
+// public and private in C++ Class
+
+#include <iostream>
+using namespace std;
+
+class Room {
+
+   private:
+    double length;
+    double breadth;
+    double height;
+
+   public:
+
+    // function to initialize private variables
+    void initData(double len, double brth, double hgt) {
+        length = len;
+        breadth = brth;
+        height = hgt;
+    }
+
+    double calculateArea() {
+        return length * breadth;
+    }
+
+    double calculateVolume() {
+        return length * breadth * height;
+    }
+};
+
+int main() {
+
+    // create object of Room class
+    Room room1;
+
+    // pass the values of private variables as arguments
+    room1.initData(42.5, 30.8, 19.2);
+
+    cout << "Area of Room =  " << room1.calculateArea() << endl;
+    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
+
+    return 0;
+}
